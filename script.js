4c6eb7d231fe562f104b24c6bb960cea03d2485a
Der Meyer <simon.der.meyer@gmail.com>
2018-06-16 16:56:36 +0200

    Style update and burger menu.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('hi jens!');
+
+(function() {
+
+    var burgerMenu = document.getElementById('burger_menu');
+    var sideMenu = document.getElementById('side_menu');
+    var isVisible = false;
+
+    burgerMenu.addEventListener('click', function() {
+        if (isVisible) {
+            sideMenu.style.left = '-12vw';
+            isVisible = false;
+        } else {
+            sideMenu.style.left = '0';
+            isVisible = true;
+        }
+    });
+
+}());
